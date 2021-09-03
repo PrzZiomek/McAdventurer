05490d3cde84031165a6ab872f76b9ffeb44fbbb
Przemysław Ziomek <pzz64056@gmail.com>
2021-09-03 21:04:08 +0200

    create webpack configuration for the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import IMAGE from "./Belfast.jpg";
+
+
+
+export const App = () => (
+    <>
+        <img src={IMAGE} alt="Belfast" style={{width: "100vw", height: "100vh"}} />
+    </>
+)
