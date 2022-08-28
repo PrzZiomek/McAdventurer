e43848af33a62e20e6c4ff181b90b269387fc377
Przemysław Ziomek <pzz64056@gmail.com>
2022-08-28 08:55:09 +0200

    style loaders for mobile

--- src/components/worldMap/styles/worldMapStyles.tsx
+++ src/components/worldMap/styles/worldMapStyles.tsx
@@ -7,16 +7,15 @@ export const MapStyled = styled(DivWrapper)`
     margin-left: -25px; 
     height: 100vh;
     flex-grow: 1;  
-    background-color: #DFDFCE;
-
-    > div {
-        background-color: #DFDFCE;
-    }
+    background-color: #DFDFCE; 
 
     canvas{
         width: 100% !important;
         height: 100% !important;
         margin: 0 !important;
         object-fit: cover;
+        background-color: #DFDFCE !important;
     }
+
+
 `
